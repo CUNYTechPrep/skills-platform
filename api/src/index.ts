1ddc7cc042d58fed5422320895afc921fec491da
Edgardo Molina <medgardo@gmail.com>
2025-03-25 17:27:38 -0400

    feat(api): init express server using typescript

---
+++ api/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
+  res.json({
+    message: "Welcome to the CTP Skills Platform",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`server started on port: ${port}`);
+});
