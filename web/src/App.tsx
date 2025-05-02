96cb4bbc15786286f1b1f7b8a5191e554dce7768
Kevin Orta <kevino6532@gmail.com>
2025-05-01 20:34:05 -0400

    feat: Add React Router (#16)
    * feat: Add React Router
    
    * merged from app.tsx from main
    
    * integrate existing frontend template with router

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,13 @@
+import { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
 import SideNav from "./components/SideNav";
-import PopularActivities from "./components/PopularActivities";
-import WeeklySchedule from "./components/WeeklySchedule";
 import RightContent from "./components/RightContent";
-import PersonalBests from "./components/PersonalBests";
+// function App() {
+//   return (
+//     <>
+//       {/* This is where the Navbar and other components will be rendered */}
+//
+//     </>
 
 export default function App() {
   return (
@@ -11,17 +16,8 @@ export default function App() {
         <div className="flex-shrink-0">
           <SideNav />
         </div>
-        <div className="flex flex-col flex-1 w-full p-4 md:p-8 overflow-y-auto min-h-0">
-          <PopularActivities />
-          <div className="flex flex-col md:flex-row gap-4 md:gap-8 mt-8">
-            <div className="w-full md:w-1/2">
-              <WeeklySchedule />
-            </div>
-            <div className="w-full md:w-1/2">
-              <PersonalBests />
-            </div>
-          </div>
-        </div>
+        <Outlet />{" "}
+        {/* This is where the Content (child routes) will be rendered */}
         <div className="w-full md:w-64 p-4 flex-shrink-0 overflow-y-auto min-h-0">
           <RightContent />
         </div>
